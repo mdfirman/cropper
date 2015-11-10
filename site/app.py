7c28bc9c529e06322cc39896c22b039dd1d3441f
MichaelF <mdfirman@gmail.com>
2015-11-10 17:04:27 +0000

    First crappy version of cropper now working

--- site/app.py
+++ site/app.py
@@ -19,8 +19,8 @@ data_dir = ('/home/michael/projects/butterflies/data/web_scraping/'
 
 def build_unlabelled_img_set():
     '''
-    Returns set, containing tuples of IDs of unlabelled images
-    and the corresponding sighting ID
+    Returns set of unlabelled images, consisting of tuples of
+    (sightingID, imageID)
     '''
     unlabelled_imgs = set()
 
@@ -48,52 +48,80 @@ def download_image(sighting_id, img_id):
         as_attachment=True)
 
 
+def get_new_images():
+    global unlabelled_imgs
+
+    if not unlabelled_imgs:
+        # might have run out, or might have missed one. Check all the files...
+        unlabelled_imgs = build_unlabelled_img_set()
+
+        # now check again, if still empty then we're done
+        if not unlabelled_imgs:
+            return None, None
+
+    return unlabelled_imgs.pop()
+
+
 # Define a route for the default URL, which loads the form
 @app.route('/')
 def form():
+    """
+    This is run the first time the page is loaded
+    """
     global tic, unlabelled_imgs
     tic = time()
-    new_sighting_id, new_img_id = unlabelled_imgs.pop()
-    print "in form", new_sighting_id, new_img_id
+    new_sighting_id, new_img_id = get_new_images()
     return render_template('form_submit.html', sighting_id=new_sighting_id,
         img_id=new_img_id)
+
 
 # Define a route for the action of the form, for example '/hello/'
 # We are also defining which type of requests this route is
 # accepting: POST requests in this case
-@app.route('/form_submission/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def form_submission():
+    """
+    This is run when the user clicks 'submit'
+    """
 
     global tic
 
     # get the results from the form
     results = {
         'number_butterflies': str(request.form['number_butterflies']),
-        'top_bottom': str(request.form['topbottom']),
         'time_taken': time() - tic
     }
     try:
+        results['top_bottom'] = str(request.form['topbottom'])
         results['crop'] = {
             'x': float(request.form['x']),
             'y': float(request.form['y']),
             'width': float(request.form['width']),
             'height': float(request.form['height'])
-            }
+        }
     except:
         pass
 
     # save the results to disk
-    with open('crop.yaml', 'w') as f:
+    sighting_id = request.form['sighting_id']
+    img_id = request.form['img_id']
+    savepath = data_dir + sighting_id + '/' + img_id + '_crop.yaml'
+
+    print "Saving results to ", savepath
+    with open(savepath, 'w') as f:
         yaml.dump(results, f, default_flow_style=False)
 
     # get a new image from the set of unlabelled images
-    global unlabelled_imgs
-    new_sighting_id, new_img_id = unlabelled_imgs.pop()
+    new_sighting_id, new_img_id = get_new_images()
 
-    # start the clock and render the new page
-    tic = time()
-    return render_template('form_submit.html', sighting_id=new_sighting_id,
-        img_id=new_img_id)
+    if new_sighting_id is None:
+        return render_template('form_finished.html')
+    else:
+        # start the clock and render the new page
+        tic = time()
+        return render_template('form_submit.html', sighting_id=new_sighting_id,
+            img_id=new_img_id)
+
 
 
 # Run the app :)
